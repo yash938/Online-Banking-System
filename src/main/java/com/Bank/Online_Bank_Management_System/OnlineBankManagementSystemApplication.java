035593ccf47383859dd8173d48cb1642a7d99f23
Yash Sharma <yash32860@gmail.com>
2024-11-25 13:08:42 +0530

    first commit

---
+++ src/main/java/com/Bank/Online_Bank_Management_System/OnlineBankManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Bank.Online_Bank_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bank/Online_Bank_Management_System/OnlineBankManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bank.Online_Bank_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
